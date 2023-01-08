7c491425dfb10121b8975a9df7aac9e9fb6b31cc
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2023-01-08 20:45:14 +0800

    Add files via upload

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            /*ReadKey() method to wait for the user to press the Enter key before terminating the app*/
+            Console.ReadKey();
+
+        }
+    }
+}
