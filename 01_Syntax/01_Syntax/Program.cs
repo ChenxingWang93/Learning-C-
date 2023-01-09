3f7c3571382de77c03c8abd6fc9772cee32e06b4
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2023-01-09 11:32:01 +0800

    Add files via upload

---
+++ 01_Syntax/01_Syntax/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Syntax
+{
+    internal class Program
+    {
+        /* a static method called Main
+         * that may accept an array of text as arguments 
+         */
+        static void Main(string[] args)
+        {
+            // this is a single line comment
+
+            // this is another single line comment
+
+            /* this is 
+             * a multiline
+             * comment
+             */
+
+            // this line prints a message to the console 
+            Console.WriteLine("Hello World!");
+
+            // this line stops the program and waits for user keyboard input
+            Console.ReadKey();
+
+        }
+    }
+}
