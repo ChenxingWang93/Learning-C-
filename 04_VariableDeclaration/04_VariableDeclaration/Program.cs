cfac3cc4222f40569eba3400362c5e2772fc6612
ChenXing Wang <31954987+ChenxingWang93@users.noreply.github.com>
2023-01-11 23:01:47 +0800

    Add files via upload

---
+++ 04_VariableDeclaration/04_VariableDeclaration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_VariableDeclaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaration and assignment 声明与赋值
+            decimal packedVolume = 0;
+            decimal packedy = 0;
+            Console.WriteLine(packedVolume);    //prints 0
+            Console.WriteLine(packedy);    //prints 0
+
+            //assignment 赋新值0.0M
+            packedVolume = 0.0M;
+            Console.WriteLine(packedVolume);    //prints 0.0M
+
+            //stop executing the program
+            Console.ReadKey();  
+        }
+    }
+}
